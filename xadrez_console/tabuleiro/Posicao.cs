5b44d5ab4854fc46834c8a7a6539870757510cc1
Jocimar B. A. Huss <jocimarspi@gmail.com>
2018-12-21 00:35:58 -0200

    Criação do projeto

---
+++ xadrez_console/tabuleiro/Posicao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xadrez_console.tabuleiro
+{
+    class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+    }
+}
