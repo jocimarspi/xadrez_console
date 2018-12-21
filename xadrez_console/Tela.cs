f9d84623a216c3f11a24f8aaf6ce04ef5c80c311
Jocimar B. A. Huss <jocimarspi@gmail.com>
2018-12-21 01:29:18 -0200

    Começando implementar método que imprime a tela

---
+++ xadrez_console/Tela.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Tela
+    {
+        public static void ImprimirTabuleiro(Tabuleiro tabuleiro)
+        {
+            for (int linha = 0; linha < tabuleiro.Linhas; linha++)
+            {
+                for (int coluna = 0; coluna < tabuleiro.Colunas; coluna++)
+                {
+                    Peca peca = tabuleiro.peca(linha, coluna);
+
+                    string casa = "-";
+                    if (peca != null)
+                    {
+                        casa = peca.ToString();
+                    }
+
+                    Console.Write(casa+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
